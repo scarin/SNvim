587151b93f68a1dd8e3a719ce82c59f6ff4086f3
scarinli <scarinli@tencent.com>
2022-11-13 12:35:54 +0800

    fixed

--- lua/config/bubbles.lua
+++ lua/config/bubbles.lua
@@ -43,7 +43,7 @@ require('lualine').setup {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
     lualine_b = { 'filename', 'branch' }, --, 'branch'
-    lualine_c = { 'diff', 'diagnostics' }, --  'fileformat'
+    lualine_c = { 'diagnostics' }, --  'fileformat'
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
